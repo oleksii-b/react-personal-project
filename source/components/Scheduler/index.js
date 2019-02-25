9b12a726660336c81701f0cb508e84d2dd5849c8
prochor <prochorz@gmail.com>
2019-02-26 00:20:19 +0200

    ls 7.5

--- source/components/Scheduler/index.js
+++ source/components/Scheduler/index.js
@@ -5,11 +5,93 @@ import React, { Component } from 'react';
 import Styles from './styles.m.css';
 import { api } from '../../REST'; // ! Импорт модуля API должен иметь именно такой вид (import { api } from '../../REST')
 
+//Components
+import Task from 'components/Task';
+import Checkbox from '../../theme/assets/Checkbox';
+
+
 export default class Scheduler extends Component {
+    state = {
+        newTaskNameHolder: '',
+        tasks: [
+            {id: 0, value: 'task1'},
+            {id: 1, value: 'task2'},
+            {id: 2, value: 'task2'},
+        ]
+    };
+
+    _addNewTask = (e) => {
+        e.preventDefault();
+
+        let tasks = [...this.state.tasks];
+
+        tasks.push({
+            id: `${ this.state.tasks[this.state.tasks.length - 1].id + 1}`, value: this.state.newTaskNameHolder
+        });
+
+        this.setState({
+            newTaskNameHolder: '',
+            tasks
+        });
+    };
+
+    _removeTask = (id) => () => {
+        let tasks = this.state.tasks.filter((item)=>{
+            return item.id !== id;
+        });
+
+        this.setState({ tasks });
+    };
+
+    _updateNewTaskName = (e) => {
+        this.setState({
+            newTaskNameHolder: e.target.value,
+        });
+    };
+
     render () {
+        const { newTaskNameHolder, tasks } = this.state;
+
+        const todosJSX = tasks.map((props)=>{
+            return <Task
+                _removeTask = { this._removeTask }
+                key = { props.id } {...props} />
+        });
+
         return (
             <section className = { Styles.scheduler }>
-                Планировщик: стартовая точка
+                <main>
+                    <header>
+                        <h1>Task Manager</h1>
+                        <input placeholder="Search" type="search"/>
+                    </header>
+
+                    <section>
+                        <form onSubmit={ this._addNewTask }>
+                            <input
+                                maxLength="50"
+                                placeholder="Description of my new task"
+                                type="text"
+                                value={ newTaskNameHolder }
+                                onChange={ this._updateNewTaskName }
+                            />
+                            <button type="submit">Add new task</button>
+                        </form>
+
+                        <ul>
+                            { todosJSX }
+                        </ul>
+                    </section>
+
+                    <footer>
+                        <Checkbox
+                            checked = { false }
+                            color1 = '#363636'
+                            color2 = '#fff'
+                        />
+                        <span className = { Styles.completeAllTasks }>All tasks is done</span>
+                    </footer>
+                </main>
             </section>
         );
     }
--- source/components/Task/index.js
+++ source/components/Task/index.js
@@ -4,7 +4,17 @@ import React, { PureComponent } from 'react';
 // Instruments
 import Styles from './styles.m.css';
 
+//Components
+import Checkbox from '../../theme/assets/Checkbox';
+import Remove from '../../theme/assets/Remove';
+import Edit from '../../theme/assets/Edit';
+import Star from '../../theme/assets/Star';
+
 export default class Task extends PureComponent {
+    state = {
+
+    }
+
     _getTaskShape = ({
         id = this.props.id,
         completed = this.props.completed,
@@ -17,7 +27,64 @@ export default class Task extends PureComponent {
         message,
     });
 
+    _toggleTaskCompletedState = () => {
+        const { completed } = this.props;
+
+    };
+
     render () {
-        return <li className = { Styles.task }>Задача: стартовая точка</li>;
+        const { completed, id, value, _removeTask } = this.props;
+
+        return (
+            <>
+                <li className = { Styles.task }>
+                    <div className = { Styles.content }>
+                        <Checkbox
+                            inlineBlock
+                            checked = { completed }
+                            className = { Styles.toggleTaskCompletedState }
+                            color1 = '#3B8EF3'
+                            color2 = 'white'
+                            onClick = { this._toggleTaskCompletedState }
+                        />
+                        <input
+                            disabled
+                            maxLength="50"
+                            type="text"
+                            value= { value }
+                        />
+                    </div>
+
+                    <div className = { Styles.actions }>
+                        <Star
+                            inlineBlock
+                            checked = { true }
+                            className = { Styles.toggleTaskFavoriteState }
+                            color1 = '#3B8EF3'
+                            color2 = '#000'
+                        />
+                        <Edit
+                            inlineBlock
+                            checked = { true }
+                            className = { Styles.updateTaskMessageOnClick }
+                            color1 = '#3B8EF3'
+                            color2 = '#000'
+                        />
+                        <Remove
+                            inlineBlock
+                            className = { Styles.removeTask }
+                            color1 = '#3B8EF3'
+                            color2 = '#000'
+                            onClick = { _removeTask( id ) }
+                        />
+                    </div>
+
+
+                </li>
+                <div className = { Styles.completed } >
+                    <div className = { Styles.content } ></div>
+                </div>
+            </>
+        );
     }
 }
